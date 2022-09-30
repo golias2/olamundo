953a26f3194734fd3718532c52aa28a58c946642
golias2 <r.felipe94@gmail.com>
2022-09-30 00:12:16 -0300

    Add files via upload

---
+++ olamundo.js
@@ -0,0 +1,121 @@
+function validateForm() {
+  let x = document.forms["myForm"]["fname"].value;
+  if (x == "7991") {
+    alert("Oi Cah! Você foi promovida à minha nova prima! 😯");
+    alert("Por favor, não conte pra ninguém ainda, principalmente no grupo!!!!");
+    alert("Sim!!! A tia Mariana e o tio Ronaldo estão grávidos 😍👼");
+    alert("Espero que tenha gostado da surpresa. Te amamos ❤");
+    alert("Ah, e logo tô por aí pra alegrar toda a família 🤗🤩");
+    return true;
+  } else if (x == "0640") {
+  	alert("Oi Cris! Você foi promovida à minha nova titia! 😯");
+    alert("Por favor, não conte pra ninguém ainda, principalmente no grupo!!!!");
+    alert("Sim!!! Sua cunhada Mariana e seu irmão Ronaldo estão grávidos 😍👼");
+    alert("Espero que tenha gostado da surpresa. Te amamos ❤");
+    alert("Ah, e logo tô por aí pra alegrar toda a família 🤗🤩");
+    return true;
+  } else if (x == "1898") {
+  	alert("Oi Lyra! Você foi promovida à minha nova prima! 😯");
+    alert("Por favor, não conte pra ninguém ainda, principalmente no grupo!!!!");
+    alert("Sim!!! A tia Mariana e o tio Ronaldo estão grávidos 😍👼");
+    alert("Espero que tenha gostado da surpresa. Te amamos ❤");
+    alert("Ah, e logo tô por aí pra alegrar toda a família 🤗🤩");
+    return true;
+  } else if (x == "8331") {
+  	alert("Oi Dan! Você foi promovido à meu novo titio! 😯");
+    alert("Por favor, não conte pra ninguém ainda, principalmente no grupo!!!!");
+    alert("Sim!!! Sua cunhada Mariana e seu irmão Ronaldo estão grávidos 😍👼");
+    alert("Espero que tenha gostado da surpresa. Te amamos ❤");
+    alert("Ah, e logo tô por aí pra alegrar toda a família 🤗🤩");
+    return true;
+  } else if (x == "9248") {
+  	alert("Oi João! Você foi promovido à meu novo primo! 😯");
+    alert("Por favor, não conte pra ninguém ainda, principalmente no grupo!!!!");
+    alert("Sim!!! A tia Mariana e o tio Ronaldo estão grávidos 😍👼");
+    alert("Espero que tenha gostado da surpresa. Te amamos ❤");
+    alert("Ah, e logo tô por aí pra alegrar toda a família 🤗🤩");
+    return true;
+  } else if (x == "6267") {
+  	alert("Oi Silvia! Você foi promovida à minha vovó! 😯");
+    alert("Por favor, não conte pra ninguém ainda, principalmente no grupo!!!!");
+    alert("Sim!!! Sua nora Mariana e seu filho Ronaldo estão grávidos 😍👼");
+    alert("Espero que tenha gostado da surpresa. Te amamos ❤");
+    alert("Ah, e logo tô por aí pra alegrar toda a família 🤗🤩");
+    return true;
+  } else if (x == "3606") {
+  	alert("Oi Mariana! Você foi promovida à minha nova mamãe! 😯");
+    alert("Por favor, não conte pra ninguém ainda, principalmente no grupo!!!!");
+    alert("Sim!!! Nós estamos grávidos 😍👼");
+    alert("Espero que tenha gostado da surpresa. Te amamos ❤");
+    alert("Ah, e logo tô por aí pra alegrar toda a família 🤗🤩");
+    return true;
+  } else if (x == "9661") {
+  	alert("Oi Theu! Você foi promovido à meu novo titio! 😯");
+    alert("Por favor, não conte pra ninguém ainda, principalmente no grupo!!!!");
+    alert("Sim!!! Sua cunhada Mariana e seu irmão Ronaldo estão grávidos 😍👼");
+    alert("Espero que tenha gostado da surpresa. Te amamos ❤");
+    alert("Ah, e logo tô por aí pra alegrar toda a família 🤗🤩");
+    return true;
+  } else if (x == "6812") {
+  	alert("Oi Rafa! Você foi promovido à meu novo titio! 😯");
+    alert("Por favor, não conte pra ninguém ainda, principalmente no grupo!!!!");
+    alert("Sim!!! Sua cunhada Mariana e seu irmão Ronaldo estão grávidos 😍👼");
+    alert("Espero que tenha gostado da surpresa. Te amamos ❤");
+    alert("Ah, e logo tô por aí pra alegrar toda a família 🤗🤩");
+    return true;
+  } else if (x == "9627") {
+  	alert("Oi Rafa! Você foi promovida à minha nova titia! 😯");
+    alert("Por favor, não conte pra ninguém ainda, principalmente no grupo!!!!");
+    alert("Sim!!! Sua cunhada Mariana e seu cunhado Ronaldo estão grávidos 😍👼");
+    alert("Espero que tenha gostado da surpresa. Te amamos ❤");
+    alert("Ah, e logo tô por aí pra alegrar toda a família 🤗🤩");
+    return true;
+  } else if (x == "4264") {
+  	alert("Oi Tamires! Você foi promovida à minha nova titia! 😯");
+    alert("Por favor, não conte pra ninguém ainda, principalmente no grupo!!!!");
+    alert("Sim!!! Sua cunhada Mariana e seu cunhado Ronaldo estão grávidos 😍👼");
+    alert("Espero que tenha gostado da surpresa. Te amamos ❤");
+    alert("Ah, e logo tô por aí pra alegrar toda a família 🤗🤩");
+    return true;
+  } else if (x == "4321") {
+  	alert("Oi Wagner! Você foi promovido à meu novo titio! 😯");
+    alert("Por favor, não conte pra ninguém ainda, principalmente no grupo!!!!");
+    alert("Sim!!! Sua cunhada Mariana e seu cunhado Ronaldo estão grávidos 😍👼");
+    alert("Espero que tenha gostado da surpresa. Te amamos ❤");
+    alert("Ah, e logo tô por aí pra alegrar toda a família 🤗🤩");
+    return true;
+  } else if (x == "0958") {
+  	alert("Oi João! Você foi promovido à meu novo primo! 😯");
+    alert("Por favor, não conte pra ninguém ainda, principalmente no grupo!!!!");
+    alert("Sim!!! A tia Mariana e o tio Ronaldo estão grávidos 😍👼");
+    alert("Espero que tenha gostado da surpresa. Te amamos ❤");
+    alert("Ah, e logo tô por aí pra alegrar toda a família 🤗🤩");
+    return true;
+  } else if (x == "4326") {
+  	alert("Oi Fuca! Você foi promovido à meu novo vovô! 😯");
+    alert("Por favor, não conte pra ninguém ainda, principalmente no grupo!!!!");
+    alert("Sim!!! Sua filha Mariana e seu genro Ronaldo estão grávidos 😍👼");
+    alert("Espero que tenha gostado da surpresa. Te amamos ❤");
+    alert("Ah, e logo tô por aí pra alegrar toda a família 🤗🤩");
+    return true;
+  } else if (x == "6524") {
+  	alert("Oi Gonça! Você foi promovido à minha nova vovó! 😯");
+    alert("Por favor, não conte pra ninguém ainda, principalmente no grupo!!!!");
+    alert("Sim!!! Sua filha Mariana e seu genro Ronaldo estão grávidos 😍👼");
+    alert("Espero que tenha gostado da surpresa. Te amamos ❤");
+    alert("Ah, e logo tô por aí pra alegrar toda a família 🤗🤩");
+    return true;
+  } else if (x == "4605") {
+  	alert("Oi Adelino! Você foi promovido à meu novo vovô! 😯");
+    alert("Por favor, não conte pra ninguém ainda, principalmente no grupo!!!!");
+    alert("Sim!!! Sua nora Mariana e seu filho Ronaldo estão grávidos 😍👼");
+    alert("Espero que tenha gostado da surpresa. Te amamos ❤");
+    alert("Ah, e logo tô por aí pra alegrar toda a família 🤗🤩");
+    return true;
+  } else {
+  	alert("Número inválido!");
+  	alert("Digite os 4 últimos digitos do número do seu celular!!");
+  	document.getElementById('fname').value = "";
+  	return false;
+  }
+}
